cae7965462768c95ab83d480cfc2e3e3775e3aa9
norit <bart.daude@gmail.com>
2018-11-20 16:55:23 +0100

    Première version du header 'connecte'

--- AngularPart/src/app/views/connecte/connecte.component.ts
+++ AngularPart/src/app/views/connecte/connecte.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { Prof } from 'src/app/model/prof';
 
 @Component({
   selector: 'app-connecte',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connecte.component.css']
 })
 export class ConnecteComponent implements OnInit {
+  connecte: boolean;
+  prof: Prof;
 
-  constructor() { }
+  constructor(
+    private storageService: LocalStorageService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+    //Par defaut on affiche pas le component
+    this.connecte = false;
+    //Il y a un prof enregistré dans le localSotarge
+    if(this.storageService.readLocal('Prof') != undefined){
+      this.connecte = true;
+      this.prof = this.storageService.readLocal('Prof');
+    } 
+    //Il y a un prof enregistré dans le sessionStorage
+    else if(this.storageService.readSession('Prof') != undefined){
+      this.connecte = true;
+      this.prof = this.storageService.readSession('Prof');
+    }
+    //Il n'y a personne
+    else{
+    }
+  }
+
+  deconnexion(){
+    this.storageService.clear();
+    this.router.navigate(['']);
   }
 
 }
